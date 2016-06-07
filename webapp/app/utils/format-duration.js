eff23ac14eed7d6654e778eb5a3836b36ccb3aa6
Eric Cao <eric.cao@nanocloud.com>
2016-06-07 22:52:20 +0200

    use moment humanize method to display time with a nice formatted text

--- webapp/app/utils/format-duration.js
+++ webapp/app/utils/format-duration.js
@@ -1,13 +1,3 @@
 export default function formatDuration(value) {
-  
-  if (value < 60) {
-    return Number(value) + ' s';
-  }
-  if (value < (60 * 60)) {
-    return Number(value/60).toFixed(2) + ' min';
-  }
-  if (value < (60 * 60 * 60)) {
-    return Number(value/60/60).toFixed(2) + ' h';
-  }
-  return Number(value/60/60/60).toFixed(2) + ' d';
+  return window.moment.duration(value, "seconds").humanize();
 }
